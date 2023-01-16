e480603cfd112968c90d9e6e2835c1f6e7f576d0
Karen Penedo <penedok12@gmail.com>
2023-01-16 15:06:49 -0400

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,21 @@
+const texto1 = 'Olá Mundo';
+const citacao = 'Meu nome é ';
+const nome = "Karen";
+
+
+
+//concatenação (+)
+citacao + nome
+
+// template string ou template literal
+
+//unicode 
+const cifrao= '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
